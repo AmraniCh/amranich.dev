50a1c71507504f22c9432a0d57de18eb682b665c
AmraniCh <elamrani.sv.laza@gmail.com>
2021-08-25 19:13:47 +0100

    extending the defaults rather than override it

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,25 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   purge: [
     "source/**/*.blade.php",
-    "source/**/*.md", 
-    "source/**/*.html",
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'black': '#444444',
+        'brown': '#8A6445',
+      },
+      backgroundImage: theme => ({
+        'glass': "url('/assets/images/glass-bg.png')"
+      }),
       backgroundSize: {
         'fit': '100% 100%',
       },
       letterSpacing: {
         'widest-x2': '.25em',
       },
-    },
-    backgroundImage: theme => ({
-      'glass': "url('/assets/images/glass-bg.png')",
-      'me': "url('/assets/images/me.png')"
-    }),
-    colors: {
-      'black': '#444444',
-      'brown': '#8A6445',
     },
   },
   variants: {
