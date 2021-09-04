05c1997bba93b284621c7aa88fe68348ec08833b
AmraniCh <elamrani.sv.laza@gmail.com>
2021-09-04 04:23:28 +0100

    Favicons Added

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -14,7 +14,32 @@
         <title>{{ $page->title ?  $page->title . ' | ' : '' }}{{ $page->siteName }}</title>
 
         <link rel="home" href="{{ $page->baseUrl }}">
-        <link rel="icon" href="/favicon.ico">
+
+        <link rel="shortcut icon" type="image/x-icon" href="/assets/icons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/favicon-96x96.png" />
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/assets/icons/apple-57x57-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/assets/icons/apple-60x60-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/assets/icons/apple-72x72-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/assets/icons/apple-76x76-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/assets/icons/apple-114x114-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/assets/icons/apple-120x120-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/assets/icons/apple-144x144-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/assets/icons/apple-152x152-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-180x180-touch-icon.png" />
+
+        <link rel="icon" type="image/png" sizes="196x196" href="/assets/icons/android-chrome-196x196.png" />
+
+        <meta name="theme-color" content="#e7d8b1" />
+        
+        <meta name="msapplication-TileColor" content="#e7d8b1" />
+        <meta name="msapplication-TileImage" content="/assets/icons/windows-tile.png">
+        <meta name="msapplication-square70x70logo" content="/assets/icons/windows-small-tile.png" />
+        <meta name="msapplication-square150x150logo" content="/assets/icons/windows-medium-tile.png" />
+        <meta name="msapplication-wide310x150logo" content="/assets/icons/windows-wide-tile.png" />
+        <meta name="msapplication-square310x310logo" content="/assets/icons/windows-large-tile.png" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -26,5 +51,6 @@
 
         @yield('body')
 
+        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
     </body>
 </html>
--- source/index.blade.php
+++ source/index.blade.php
@@ -7,13 +7,13 @@
     <h1 class="text-2xl">amranich.dev</h1>
     <p class="text-4xl py-2 font-medium text-brown sm:text-6xl sm:pt-2">I am almost there!</p>
     <div class="py-2 flex flex-col align-start sm:flex-row sm:space-x-12 sm:pt-2">
-        <a class="tracking-widest-x2" href="{{ $page->links->github }}" target="blank">GitHub 
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block bounce-rigth-animation" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <a class="tracking-widest-x2" href="{{ $page->links->github }}" target="blank">GitHub
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block bounce-right-animation" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 A<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
             </svg>
         </a>
-        <a class="tracking-widest-x2" href="{{ $page->links->behance }}" target="blank">Behance 
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block bounce-rigth-animation" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <a class="tracking-widest-x2" href="{{ $page->links->behance }}" target="blank">Behance
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block bounce-right-animation" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
             </svg>
         </a>
