5ccaeb37964b2aea6e255d6ccc21629883bde9ce
AmraniCh <elamrani.sv.laza@gmail.com>
2022-03-17 23:29:30 +0100

    feat: [landing page]: Added linkedin link

--- config.php
+++ config.php
@@ -10,6 +10,7 @@ return [
     'links' => [
         'github' => 'https://github.com/amranich',
         'behance' => 'https://www.behance.net/amranich',
+        'linkedin' => 'https://www.linkedin.com/in/el-amrani-chakir/',
     ],
     'email' => 'contact@amranich.dev',
 ];
--- source/index.blade.php
+++ source/index.blade.php
@@ -12,6 +12,11 @@
                 A<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
             </svg>
         </a>
+        <a class="tracking-widest-x2" href="{{ $page->links->linkedin }}" target="blank">Linkedin
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block bounce-right-animation" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                A<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+            </svg>
+        </a>
         <a class="tracking-widest-x2" href="{{ $page->links->behance }}" target="blank">Behance
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block bounce-right-animation" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
