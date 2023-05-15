134fe78b5b8fa1668b4f19ac0b9969ae792d34af
AmraniCh <elamrani.sv.laza@gmail.com>
2023-05-15 20:41:27 +0100

    style changes for coming soon page

--- source/app-down.blade.php
+++ source/app-down.blade.php
@@ -1,13 +1,13 @@
 @extends('_layouts.main')
 
 @section('body')
-    <div class="h-screen bg-yellow p-8">
-        <div class="bg-glass bg-no-repeat bg-fit flex justify-center items-center h-full relative rounded-lg">
+    <div class="bg-yellow p-8 h-full">
+        <div class="bg-glass bg-no-repeat bg-fit flex justify-center items-center h-full relative rounded-lg drop-shadow-lg">
             <div class="flex flex-col justify-center items-center text-black">
                 <img class="w-24 mb-4 sm:mb-0 sm:absolute sm:top-8 sm:left-8 select-none" src="assets/images/logo.svg" alt="El Amrani Chakir">
                 <img class="rounded-full mb-2 w-40" src="assets/images/me.jpg" alt="El Amrani Chakir">
                 <h1 class="text-2xl tracking-widest">amranich.dev</h1>
-                <p class="text-4xl py-4 font-medium text-brown sm:text-6xl sm:pt-2 tracking-wide">I am almost there!</p>
+                <p class="text-4xl py-4 font-medium text-brown text-center sm:text-6xl sm:pt-2 tracking-wide">I am almost there!</p>
                 <div class="py-2 flex flex-col align-start sm:flex-row sm:space-x-12 sm:pt-2">
                     <a class="tracking-widest-x2 font-medium" href="{{ $page->links->github }}" target="blank">GitHub
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block bounce-right-animation"
