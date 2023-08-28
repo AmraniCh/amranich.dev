933ce3cc634b4fa6e5170e4760cc215bee5dc25e
AmraniCh <elamrani.sv.laza@gmail.com>
2023-08-29 00:33:33 +0100

    feat: added career section

--- source/_index.blade.php
+++ source/_index.blade.php
@@ -2,11 +2,12 @@
 
 @section('body')
     <div
-        class="container bg-white max-w-7xl m-auto py-4 dark:bg-[#222] lg:py-8 rounded-lg drop-shadow-sm shadow-black h-[3000px]">
+        class="container bg-white max-w-7xl m-auto py-4 dark:bg-[#222] lg:py-8 rounded-lg drop-shadow-sm shadow-black h-[3000px] overflow-hidden">
 
         @include('_partials.hero')
         @include('_partials.about-me')
         @include('_partials.care-about')
+        @include('_partials.career')
 
     </div>
 @endsection
---
+++ source/_partials/career.blade.php
@@ -0,0 +1,53 @@
+<div class="section px-8 lg:px-16 bg-[#fdf0d259] dark:lg:bg-[#2c2c2c] bg-cover">
+    <x-section-heading text="{!! $page->trans('career.heading') !!}" />
+
+    <div class="flex flex-col gap-9">
+        <div class="w-full bgx-[#fff8e5c9] -mx-5 -my-3 px-5 py-3 rounded hover:bg-white group">
+            <div class="flex justify-between items-center">
+                <div>
+                    <h3 class="capitalize text-2xl font-medium group-hover:text-yellow">mid level software developer</h3>
+                    <div class="mt-3">
+                        <p class="text-md">PI Marketing · Full-time</p>
+                        <p class="text-sm mt-1">Nov 2021 - Present</p>
+                        <p class="text-xs mt-1">Tangier, Morocco</p>
+                    </div>
+                </div>
+                <div class="mr-4">
+                    <img class="w-20 border shadow-xs p-1" src="https://media.licdn.com/dms/image/C560BAQFHylUm39Mmjw/company-logo_200_200/0/1577814217673?e=1701302400&v=beta&t=sIUeZjrniO7trUaSnW4BrgaL2hyH8XwEcSr1DGUHkXg" alt="PI Mareting">
+                </div>
+            </div>
+        </div>
+        
+        <div class="w-full bgx-[#fff8e5c9] -mx-5 -my-3 px-5 py-3 rounded hover:bg-white group">
+            <div class="flex justify-between items-center">
+                <div>
+                    <h3 class="capitalize text-2xl font-medium group-hover:text-yellow">full stuck developer</h3>
+                    <div class="mt-3">
+                        <p class="text-md">Map-concepts · Internship</p>
+                        <p class="text-sm mt-1">Jan 2021 - Jun 2021</p>
+                        <p class="text-xs mt-1">Tangier, Morocco</p>
+                    </div>
+                </div>
+                <div class="mr-4">
+                    <img class="w-20 border shadow-xs p-1" src="https://media.licdn.com/dms/image/C510BAQETEj-EWFZNRw/company-logo_200_200/0/1519910422356?e=1701302400&v=beta&t=Yf1wrmJ-4_6BzaCFQ_xoI04DDai_iiYp64Ojnzf0oLI" alt="Map-concepts">
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full bgx-[#fff8e5c9] -mx-5 -my-3 px-5 py-3 rounded hover:bg-white group">
+            <div class="flex justify-between items-center">
+                <div>
+                    <h3 class="capitalize text-2xl font-medium group-hover:text-yellow">bachelor's degree, software and web development</h3>
+                    <div class="mt-3">
+                        <p class="text-md">Université Abdelmalek Essaâdi</p>
+                        <p class="text-sm mt-1">2017 - 2020</p>
+                        <p class="text-xs mt-1">Tetouan, Morocco</p>
+                    </div>
+                </div>
+                <div class="mr-4">
+                    <img class="w-20 border shadow-xs p-1" src="https://media.licdn.com/dms/image/C4D0BAQH8AUEANxGGXQ/company-logo_200_200/0/1525282007615?e=1701302400&v=beta&t=5CNnO3zQ6_kIzgtJTEcz_F6LdCvP0ICBoPDmI4WbB6k" alt="Université Abdelmalek Essaâdi">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
