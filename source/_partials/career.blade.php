75392af739ba4e1345f29cf760a2d75f620e0767
AmraniCh <elamrani.sv.laza@gmail.com>
2023-10-08 18:04:18 +0100

    career section enhancements

--- source/_partials/career.blade.php
+++ source/_partials/career.blade.php
@@ -1,11 +1,11 @@
-<div class="section px-8 lg:px-16 bg-[#fdf0d259] dark:lg:bg-[#2c2c2c] bg-cover">
+<div class="section px-8 lg:px-16 bg-[#fdf0d259] dark:lg:bg-[#2c2c2c]">
     <x-section-heading text="{!! $page->trans('career.heading') !!}" />
 
-    <div class="flex flex-col gap-9">
-        <div class="w-full bgx-[#fff8e5c9] -mx-5 -my-3 px-5 py-3 rounded hover:bg-white group">
+    <div class="flex flex-col gap-12">
+        <div class="w-full bgx-[#fff8e5c9] -mx-5 -my-3 px-5 py-3 rounded hover:bg-bone/20 bg-white group dark:bg-[#fff8e1] dark:hover:bg-white shadow-sm">
             <div class="flex justify-between items-center">
                 <div>
-                    <h3 class="capitalize text-2xl font-medium group-hover:text-yellow">mid level software developer</h3>
+                    <h3 class="capitalize text-2xl font-medium text-yellow">mid level software developer</h3>
                     <div class="mt-3">
                         <p class="text-md">PI Marketing · Full-time</p>
                         <p class="text-sm mt-1">Nov 2021 - Present</p>
@@ -13,15 +13,15 @@
                     </div>
                 </div>
                 <div class="mr-4">
-                    <img class="w-20 border shadow-xs p-1" src="https://media.licdn.com/dms/image/C560BAQFHylUm39Mmjw/company-logo_200_200/0/1577814217673?e=1701302400&v=beta&t=sIUeZjrniO7trUaSnW4BrgaL2hyH8XwEcSr1DGUHkXg" alt="PI Mareting">
+                    <img class="max-w-[5rem] border shadow-xs p-1" src="https://media.licdn.com/dms/image/C560BAQFHylUm39Mmjw/company-logo_200_200/0/1577814217673?e=1701302400&v=beta&t=sIUeZjrniO7trUaSnW4BrgaL2hyH8XwEcSr1DGUHkXg" alt="PI Mareting">
                 </div>
             </div>
         </div>
         
-        <div class="w-full bgx-[#fff8e5c9] -mx-5 -my-3 px-5 py-3 rounded hover:bg-white group">
+        <div class="w-full bgx-[#fff8e5c9] -mx-5 -my-3 px-5 py-3 rounded hover:bg-bone/20 bg-white group dark:bg-[#fff8e1] dark:hover:bg-white shadow-sm">
             <div class="flex justify-between items-center">
                 <div>
-                    <h3 class="capitalize text-2xl font-medium group-hover:text-yellow">full stuck developer</h3>
+                    <h3 class="capitalize text-2xl font-medium text-yellow">full stuck developer</h3>
                     <div class="mt-3">
                         <p class="text-md">Map-concepts · Internship</p>
                         <p class="text-sm mt-1">Jan 2021 - Jun 2021</p>
@@ -29,25 +29,29 @@
                     </div>
                 </div>
                 <div class="mr-4">
-                    <img class="w-20 border shadow-xs p-1" src="https://media.licdn.com/dms/image/C510BAQETEj-EWFZNRw/company-logo_200_200/0/1519910422356?e=1701302400&v=beta&t=Yf1wrmJ-4_6BzaCFQ_xoI04DDai_iiYp64Ojnzf0oLI" alt="Map-concepts">
+                    <img class="max-w-[5rem] border shadow-xs p-1" src="https://media.licdn.com/dms/image/C510BAQETEj-EWFZNRw/company-logo_200_200/0/1519910422356?e=1701302400&v=beta&t=Yf1wrmJ-4_6BzaCFQ_xoI04DDai_iiYp64Ojnzf0oLI" alt="Map-concepts">
                 </div>
             </div>
         </div>
 
-        <div class="w-full bgx-[#fff8e5c9] -mx-5 -my-3 px-5 py-3 rounded hover:bg-white group">
+        <div class="w-full bgx-[#fff8e5c9] -mx-5 -my-3 px-5 py-3 rounded hover:bg-bone/20 bg-white group dark:bg-[#fff8e1] dark:hover:bg-white shadow-sm">
             <div class="flex justify-between items-center">
                 <div>
-                    <h3 class="capitalize text-2xl font-medium group-hover:text-yellow">bachelor's degree, software and web development</h3>
+                    <h3 class="capitalize text-2xl font-medium text-yellow">bachelor's degree, software and web development</h3>
                     <div class="mt-3">
-                        <p class="text-md">Université Abdelmalek Essaâdi</p>
+                        <p class="text-md">Abdelmalek Essaâdi University</p>
                         <p class="text-sm mt-1">2017 - 2020</p>
                         <p class="text-xs mt-1">Tetouan, Morocco</p>
                     </div>
                 </div>
                 <div class="mr-4">
-                    <img class="w-20 border shadow-xs p-1" src="https://media.licdn.com/dms/image/C4D0BAQH8AUEANxGGXQ/company-logo_200_200/0/1525282007615?e=1701302400&v=beta&t=5CNnO3zQ6_kIzgtJTEcz_F6LdCvP0ICBoPDmI4WbB6k" alt="Université Abdelmalek Essaâdi">
+                    <img class="max-w-[5rem] border shadow-xs p-1" src="https://media.licdn.com/dms/image/C4D0BAQH8AUEANxGGXQ/company-logo_200_200/0/1525282007615?e=1701302400&v=beta&t=5CNnO3zQ6_kIzgtJTEcz_F6LdCvP0ICBoPDmI4WbB6k" alt="Université Abdelmalek Essaâdi">
                 </div>
             </div>
         </div>
     </div>
+
+    <span class="block mt-8 text-sm text-bone font-medium italic">
+        * visit my <a href="{{ $page->links->linkedin }}" target="_blank" class="text-blue-300 underline">Linkedin</a> profile for more information about my career.
+    </span>
 </div>
