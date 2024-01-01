9e8f07bb2437a86e9e439a68cd72f13d6e5442e8
Shakir El Amrani <elamrani.sv.laza@gmail.com>
2024-01-01 18:33:46 +0100

    coming soon: new enhancements

--- source/_index.blade.php
+++ source/_index.blade.php
@@ -1,13 +1,13 @@
 @extends('_layouts.app-down')
 
 @section('body')
-    <div class="bg-yellow p-8 h-full">
-        <div class="bg-glass bg-no-repeat bg-fit flex justify-center items-center h-full relative rounded-lg drop-shadow-lg">
+    <div class="bg-yellow p-2 sm:p-8 h-full">
+        <div class="bg-glass bg-no-repeat bg-fit flex justify-center items-center h-full relative rounded-2xl drop-shadow-md">
             <div class="flex flex-col justify-center items-center text-yellow-900">
-                <img class="w-24 mb-4 sm:mb-0 sm:absolute sm:top-8 sm:left-8 select-none" src="assets/images/logo.svg" alt="El Amrani Chakir">
-                <img class="rounded-full mb-2 w-40" src="/assets/images/me.png" alt="El Amrani Chakir">
-                <h1 class="text-xl tracking-widest sm:text-2xl">amranich.dev</h1>
-                <p class="text-3xl py-3 font-bold text-brown text-center tracking-widest sm:text-6xl sm:py-4">COMING SOON..</p>
+                <img class="w-24 mb-4 select-none sm:mb-0 sm:absolute sm:top-8 sm:left-8 sm:w-28" src="assets/images/logo.svg" alt="El Amrani Chakir">
+                <img class="rounded-full mb-2 w-44" src="/assets/images/me.png" alt="El Amrani Chakir">
+                <h1 class="text-xl tracking-wider sm:text-3xl">amranich.dev</h1>
+                <p class="text-3xl py-3 font-bold text-brown text-center tracking-widest sm:text-6xl sm:py-6">COMING SOON..</p>
                 <div class="flex flex-col align-start sm:flex-row sm:space-x-12">
                     <a class="flex items-center gap-2 tracking-widest-x2 text-sm font-medium" href="{{ $page->links->github }}" target="blank">
                         <svg class="w-4 h-4 fill-yellow text-yellow"
