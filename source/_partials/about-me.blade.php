9f1f8e69f8457b7922a465b18c241897f34547e6
Shakir El Amrani <elamrani.sv.laza@gmail.com>
2024-01-01 22:45:11 +0100

    about me: changed text

--- source/_partials/about-me.blade.php
+++ source/_partials/about-me.blade.php
@@ -5,42 +5,22 @@
 
         <div class="flex flex-col gap-6">
             <p
-                class="text-md leading-7 md:leading-loose xl:leading-loose md:text-lg xl:text-xl relative md:ml-4">
-                {{-- {{ $page->trans('about.content') }} --}}
-
-
-                I am a software developer based in Tangier, Morocco. My 3 years of software development experience has
-                given me a strong foundation for building modern software and complex web solutions.
-
+                class="first-letter:text-5xl text-md text-justify leading-7 md:leading-loose xl:leading-loose md:text-lg xl:text-xl md:ml-4">
+                {{ $page->trans('about.content.1') }}
             </p>
 
             <p
-                class="text-md leading-7 md:leading-loose xl:leading-loose md:text-lg xl:text-xl relative md:ml-4">
-                {{-- {{ $page->trans('about.content') }} --}}
-
-
-                Recently, I graduated with a Bachelor's degree in Software and Web Development with honors. I am enjoy
-                learning how things works behind the scenes and solving problems.
-
-
+                class="text-md text-justify leading-7 md:leading-loose xl:leading-loose md:text-lg xl:text-xl md:ml-4">
+                {{ $page->trans('about.content.2') }}
             </p>
 
             <p
-                class="text-md leading-7 md:leading-loose xl:leading-loose md:text-lg xl:text-xl relative md:ml-4">
-                {{-- {{ $page->trans('about.content') }} --}}
-
-
-                I am also very passionate about open source software, and I've always been excited to share my knowledge
-                with other developers and work together to build things that benefit the world.
+                class="text-md text-justify leading-7 md:leading-loose xl:leading-loose md:text-lg xl:text-xl md:ml-4">
+                {{ $page->trans('about.content.3') }}
             </p>
         </div>
 
-
-
-
-
         <div>
-            {{-- <h5 class="mb-6">My Favorite Quote: </h5> --}}
             <x-blockquote-ship />
         </div>
     </div>
