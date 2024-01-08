aa94945392d4992d61f8914c2a560cd6578d7875
Shakir El Amrani <elamrani.sv.laza@gmail.com>
2024-01-08 21:43:23 +0100

    featured-projects: put the final touches

--- source/_components/featured-project.blade.php
+++ source/_components/featured-project.blade.php
@@ -4,7 +4,7 @@
         <!-- Project Snapshot -->
         <div class="lg:flex lg:items-center lg:flex-shrink-0">
             <img class="w-full lg:w-[500px] xl:w-[600px] rounded-md" src="/assets/images/{{ $snapshot }}"
-                alt="El Amrani Chakir's personal webiste snapshot">
+                alt="{{ $snapshotAlt }}">
         </div>
         <div class="mt-3 lg:flex lg:flex-col lg:justify-center">
 
@@ -12,7 +12,31 @@
             <span class="inline-block text-xs italic font-light capitalize">{!! $date !!}</span>
 
             <!-- Title -->
-            <h5 class="mt-2 capitalize">{!! $title !!}</h5>
+            <h5 class="mt-2 capitalize flex items-center gap-2">
+                {!! $title !!}
+                @if (isset($highlight))
+                    <div class="flex items-center gap-1">
+                        <svg class="fill-yellow-900 w-4 h-4" xmlns="http://www.w3.org/2000/svg" height="16"
+                            width="18"
+                            viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path
+                                d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                        </svg>
+                        <svg class="fill-yellow-900 w-4 h-4" xmlns="http://www.w3.org/2000/svg" height="16"
+                            width="18"
+                            viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path
+                                d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                        </svg>
+                        <svg class="fill-yellow-900 w-4 h-4" xmlns="http://www.w3.org/2000/svg" height="16"
+                            width="18"
+                            viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path
+                                d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                        </svg>
+                    </div>
+                @endif
+            </h5>
             <h3 class="mt-3 text-lg font-bold tracking-wider lg:text-2xl">
                 {!! $projectTitle !!}
             </h3>
--- source/_partials/featured-projects.blade.php
+++ source/_partials/featured-projects.blade.php
@@ -3,7 +3,8 @@
 
     <div class="flex flex-col gap-10 xl:gap-16">
 
-        <x-featured-project snapshot="personal-webiste-snapshot.jpg" date="{!! $page->trans('featured-projects.first-project.date') !!}"
+        <x-featured-project snapshot="personal-webiste-snapshot.jpg"
+            snapshotAlt="Shakir El Amrani's personal website snapshot" date="{!! $page->trans('featured-projects.first-project.date') !!}"
             title="{!! $page->trans('featured-projects.card-title') !!}" projectTitle="{!! $page->trans('featured-projects.first-project.title') !!}" projectDesc="{!! $page->trans('featured-projects.first-project.desc') !!}">
 
             <x-slot:stack>
@@ -11,18 +12,20 @@
                 <x-badges.base text="Jigsaw (SSG)" />
                 <x-badges.base text="Tailwindcss" />
                 <x-badges.base text="Vanilla Js" />
+                <x-badges.base text="ES6" />
             </x-slot>
 
         </x-featured-project>
 
-        <x-featured-project highlight="" snapshot="ftp-filemanager-snapshot.png" date="{!! $page->trans('featured-projects.second-project.date') !!}"
-            title="{!! $page->trans('featured-projects.card-title') !!}" projectTitle="{!! $page->trans('featured-projects.second-project.title') !!}"
-            projectDesc="{!! $page->trans('featured-projects.second-project.desc') !!}">
+        <x-featured-project highlight="" snapshotAlt="FTP file manager web application snapshot"
+            snapshot="ftp-filemanager-snapshot.png" date="{!! $page->trans('featured-projects.second-project.date') !!}" title="{!! $page->trans('featured-projects.card-title') !!}"
+            projectTitle="{!! $page->trans('featured-projects.second-project.title') !!}" projectDesc="{!! $page->trans('featured-projects.second-project.desc') !!}">
 
             <x-slot:stack>
                 <x-badges.base text="PHP" />
                 <x-badges.base text="Apache" />
                 <x-badges.base text="MVC" />
+                <x-badges.base text="PHPUnit" />
                 <x-badges.base text="FTP" />
                 <x-badges.base text="RFC959" />
                 <x-badges.base text="SASS" />
@@ -30,13 +33,15 @@
                 <x-badges.base text="Rollup" />
                 <x-badges.base text="Gulp" />
                 <x-badges.base text="PostCss" />
+                <x-badges.base text="ES5" />
                 <x-badges.base text="Pollyfills" />
             </x-slot>
 
         </x-featured-project>
 
 
-        <x-featured-project snapshot="mysql-database-manager-snapshot.png" date="{!! $page->trans('featured-projects.third-project.date') !!}"
+        <x-featured-project snapshot="mysql-database-manager-snapshot.png"
+            snapshotAlt="MySQL Database Manager web application snapshot" date="{!! $page->trans('featured-projects.third-project.date') !!}"
             title="{!! $page->trans('featured-projects.card-title') !!}" projectTitle="{!! $page->trans('featured-projects.third-project.title') !!}"
             projectDesc="{!! $page->trans('featured-projects.third-project.desc') !!}">
 
