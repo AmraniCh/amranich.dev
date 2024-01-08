888ae2d0421863cd3fc36d2bb0135351beb24c5c
Shakir El Amrani <elamrani.sv.laza@gmail.com>
2024-01-08 22:16:47 +0100

    career: more info notice: enhancements

--- source/_partials/career.blade.php
+++ source/_partials/career.blade.php
@@ -69,8 +69,17 @@
         </div>
     </div>
 
-    <span class="block mt-8 text-sm text-brown-400 italic">
-        * {{ $page->trans('career.linkedin-notice.1') }} <a href="{{ $page->links->linkedin }}" target="_blank"
-            class="text-blue-300 underline">Linkedin</a> {{ $page->trans('career.linkedin-notice.2') }}.
+
+    <span class="mt-8 text-sm text-yellow-900 dark:text-yellow-200 italic flex items-center gap-1">
+        <svg class="fill-yellow-900 dark:fill-yellow-200" xmlns="http://www.w3.org/2000/svg" height="16"
+            width="16"
+            viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+            <path
+                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
+        </svg>
+
+        <span>{{ $page->trans('career.linkedin-notice.1') }} <a href="{{ $page->links->linkedin }}" target="_blank"
+                class="text-blue-300 underline">Linkedin</a> {{ $page->trans('career.linkedin-notice.2') }}.
+        </span>
     </span>
 </div>
