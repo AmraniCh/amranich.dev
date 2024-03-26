f8206fd2e1ab7cfe3407cea1c9bda9984423632c
Shakir El Amrani <elamrani.sv.laza@gmail.com>
2024-03-26 22:20:22 +0000

    style: featured projects: archive notice: handle responsibility in mobile

--- source/_partials/featured-projects.blade.php
+++ source/_partials/featured-projects.blade.php
@@ -56,14 +56,14 @@
         </x-project-card>
 
         <div
-            class="tip flex items-center gap-4 p-4 bg-white border border-yellow-800 rounded-lg dark:bg-dark dark:border-yellow-200">
-            <div class="flex items-center gap-2">
-                <svg class="w-10 h-10 shrink-0 fill-yellow-400 dark:fill-yellow-200" xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 30 30" width="30px" height="30px">
+            class="tip flex flex-col gap-4 p-4 bg-white border border-dashed border-yellow-800 rounded-lg lg:flex-row lg:items-center dark:bg-dark dark:border-yellow-200">
+            <div class="lg:flex md:items-center md:gap-2">
+                <svg class="inline w-6 h-6 shrink-0 fill-yellow-400 lg:w-10 lg:h-10 dark:fill-yellow-200"
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30px" height="30px">
                     <path
                         d="M 6.5292969 2.515625 A 1 1 0 0 0 5.8085938 2.8085938 A 1 1 0 0 0 5.8085938 4.2226562 A 1 1 0 0 0 7.2226562 4.2226562 A 1 1 0 0 0 7.2226562 2.8085938 A 1 1 0 0 0 6.5292969 2.515625 z M 23.5 2.515625 A 1 1 0 0 0 22.777344 2.8085938 A 1 1 0 0 0 22.777344 4.2226562 A 1 1 0 0 0 24.191406 4.2226562 A 1 1 0 0 0 24.191406 2.8085938 A 1 1 0 0 0 23.5 2.515625 z M 15 3 C 10.029 3 6 7.029 6 12 C 6 17 10 19 12 23 L 18 23 C 20 19 24 17 24 12 C 24 7.029 19.971 3 15 3 z M 15 6 L 15 11 L 19 11 L 15 18 L 15 13 L 11 13 L 15 6 z M 3 11 A 1 1 0 0 0 2 12 A 1 1 0 0 0 3 13 A 1 1 0 0 0 4 12 A 1 1 0 0 0 3 11 z M 27 11 A 1 1 0 0 0 26 12 A 1 1 0 0 0 27 13 A 1 1 0 0 0 28 12 A 1 1 0 0 0 27 11 z M 6.5292969 19.484375 A 1 1 0 0 0 5.8066406 19.777344 A 1 1 0 0 0 5.8066406 21.191406 A 1 1 0 0 0 7.2226562 21.191406 A 1 1 0 0 0 7.2226562 19.777344 A 1 1 0 0 0 6.5292969 19.484375 z M 23.498047 19.486328 A 1 1 0 0 0 22.777344 19.777344 A 1 1 0 0 0 22.777344 21.193359 A 1 1 0 0 0 24.191406 21.193359 A 1 1 0 0 0 24.191406 19.777344 A 1 1 0 0 0 23.498047 19.486328 z M 12 25 L 12 26 C 12 27.105 12.895 28 14 28 A 1 1 0 0 0 15 29 A 1 1 0 0 0 16 28 C 17.105 28 18 27.105 18 26 L 18 25 L 12 25 z" />
                 </svg>
-                <p class="text-lg">
+                <p class="inline lg:text-lg">
                     These are my "classic" projects. Though a couple are older (completed in 2020), I learned a lot from
                     them
                     and I'm
