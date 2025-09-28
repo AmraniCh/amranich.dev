f2fc03d9e84a983a12d794153d52de8959fe7e2b
Shakir El Amrani <elamrani.sv.laza@gmail.com>
2025-09-28 11:39:52 +0000

    feat: projects: add new project and update links

---
+++ source/_components/svg/figma.blade.php
@@ -0,0 +1,6 @@
+<svg class="w-5 h-5 fill-yellow-800 hover:scale-110 hover:opacity-90 dark:fill-yellow-200"
+    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path
+        d="M5 6a4 4 0 0 1 4-4h6a4 4 0 0 1 2.646 7A4 4 0 0 1 13 15.465V18a4 4 0 1 1-6.646-3A4 4 0 0 1 5 12a4 4 0 0 1 1.354-3A4 4 0 0 1 5 6m6 4H9a2 2 0 1 0 0 4h2zm2 2a2 2 0 1 0 4 0a2 2 0 0 0-4 0m2-4a2 2 0 1 0 0-4h-2v4zM9 4a2 2 0 1 0 0 4h2V4zm2 12H9a2 2 0 1 0 2 2z">
+    </path>
+</svg>
