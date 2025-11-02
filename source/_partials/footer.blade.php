9a3f5da8e2cf86b8766355b17d5ea1f0bd3776a7
AmraniCh <elamrani.sv.laza@gmail.com>
2025-11-02 11:17:34 +0100

    change: footer: update hosting and deployment information

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -6,12 +6,13 @@
                 <path
                     d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
             </svg>
-            By AmraniCh,
+            by AmraniCh
         </span>
-        <span>Hosted on Hostinger</span>
-        <span>and Deployed via FTP with Github Actions</span>
+        <span>| ☁️ Hosted on AWS |</span>
+        <span>🚀 Deployed via FTPS with GitHub Actions</span>
         @if ($page->version)
-            <a class="lg:ml-1" href="https://github.com/AmraniCh/amranich.dev/releases/tag/v0.1.8" target="_blank">
+            <a class="lg:ml-1" href="https://github.com/AmraniCh/amranich.dev/releases/tag/{{ $page->version }}"
+                target="_blank">
                 <x-badge text="{{ $page->version }}" />
             </a>
         @endif
@@ -28,5 +29,4 @@
                 fill="#286038"></path>
         </svg>
     </div>
-
 </div>
