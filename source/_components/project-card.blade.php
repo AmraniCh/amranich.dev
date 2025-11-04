d3a0962fc4d7de0e2a520c738741e9f187af2f9e
AmraniCh <elamrani.sv.laza@gmail.com>
2025-11-04 23:32:09 +0100

    style (project-card): add hover effect for image scaling

--- source/_components/project-card.blade.php
+++ source/_components/project-card.blade.php
@@ -1,10 +1,11 @@
 <div
-    class="card noscript-opacity-1 opacity-0 {{ $highlight ? 'px-4 py-8 rounded-md bg-white border-2 border-dashed border-yellow-800 dark:bg-brown-900 dark:border-yellow-200' : '' }}">
+    class="card noscript-opacity-1 opacity-0 group/container {{ $highlight ? 'px-4 py-8 rounded-md bg-white border-2 border-dashed border-yellow-800 dark:bg-brown-900 dark:border-yellow-200' : '' }}">
     <div class="flex flex-col w-full {{ $highlight ? 'lg:flex-row-reverse' : 'lg:flex-row' }} lg:gap-6">
         <!-- Project Snapshot -->
         <div class="lg:flex lg:items-center lg:flex-shrink-0">
-            <img class="w-full lg:w-[500px] xl:w-[600px] rounded-md" src="/assets/images/{{ $snapshot }}"
-                alt="{{ $snapshotAlt }}">
+            <img class="w-full rounded-md transition-transform group-hover/container:scale-[1.01] lg:w-[500px] xl:w-[600px]"
+                src="/assets/images/{{ $snapshot }}" alt="{{ $snapshotAlt }}">
+
         </div>
         <!-- Project Description -->
         <div class="mt-3 lg:flex lg:flex-col lg:justify-center">
