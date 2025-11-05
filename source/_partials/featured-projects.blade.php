147ec31f1a756fbf9d235b701fe98a415f1e3548
Shakir El Amrani <elamrani.sv.laza@gmail.com>
2025-11-05 11:06:39 +0000

    style (featured-projects): update project notice text and adjust layout

--- source/_partials/featured-projects.blade.php
+++ source/_partials/featured-projects.blade.php
@@ -63,28 +63,40 @@
             </x-slot>
         </x-project-card>
 
-        <div
-            class="tip flex flex-col items-center gap-4 p-4 bg-white border border-dashed border-yellow-800 rounded-lg dark:bg-dark dark:border-yellow-200 lg:flex-row">
-            <div class="flex flex-col items-center gap-1 lg:flex-row">
-                <svg class="inline w-8 h-8 shrink-0 fill-yellow-400 lg:w-10 lg:h-10 dark:fill-yellow-200"
-                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30px" height="30px">
-                    <path
-                        d="M 6.5292969 2.515625 A 1 1 0 0 0 5.8085938 2.8085938 A 1 1 0 0 0 5.8085938 4.2226562 A 1 1 0 0 0 7.2226562 4.2226562 A 1 1 0 0 0 7.2226562 2.8085938 A 1 1 0 0 0 6.5292969 2.515625 z M 23.5 2.515625 A 1 1 0 0 0 22.777344 2.8085938 A 1 1 0 0 0 22.777344 4.2226562 A 1 1 0 0 0 24.191406 4.2226562 A 1 1 0 0 0 24.191406 2.8085938 A 1 1 0 0 0 23.5 2.515625 z M 15 3 C 10.029 3 6 7.029 6 12 C 6 17 10 19 12 23 L 18 23 C 20 19 24 17 24 12 C 24 7.029 19.971 3 15 3 z M 15 6 L 15 11 L 19 11 L 15 18 L 15 13 L 11 13 L 15 6 z M 3 11 A 1 1 0 0 0 2 12 A 1 1 0 0 0 3 13 A 1 1 0 0 0 4 12 A 1 1 0 0 0 3 11 z M 27 11 A 1 1 0 0 0 26 12 A 1 1 0 0 0 27 13 A 1 1 0 0 0 28 12 A 1 1 0 0 0 27 11 z M 6.5292969 19.484375 A 1 1 0 0 0 5.8066406 19.777344 A 1 1 0 0 0 5.8066406 21.191406 A 1 1 0 0 0 7.2226562 21.191406 A 1 1 0 0 0 7.2226562 19.777344 A 1 1 0 0 0 6.5292969 19.484375 z M 23.498047 19.486328 A 1 1 0 0 0 22.777344 19.777344 A 1 1 0 0 0 22.777344 21.193359 A 1 1 0 0 0 24.191406 21.193359 A 1 1 0 0 0 24.191406 19.777344 A 1 1 0 0 0 23.498047 19.486328 z M 12 25 L 12 26 C 12 27.105 12.895 28 14 28 A 1 1 0 0 0 15 29 A 1 1 0 0 0 16 28 C 17.105 28 18 27.105 18 26 L 18 25 L 12 25 z" />
-                </svg>
-                <p class="text-center lg:text-left lg:text-lg">
-                    {!! $page->trans('featured-projects.notice') !!}
-                </p>
-            </div>
 
-            <x-buttons.secondary text="{!! $page->trans('featured-projects.notice.button') !!}" class="shrink-0" href="#projetcs-archive"
-                :reverseDirection="true">
-                <svg class="w-5 h-5 fill-brown-500 group-hover:fill-yellow-50 group-hover:scale-125 group-hover:duration-300 dark:fill-yellow-200 dark:group-hover:fill-yellow-900"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
-                    stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
-                </svg>
-            </x-buttons.secondary>
+
+        <div class="relative group/ctr">
+            <span
+                class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-yellow-900 group-hover/ctr:top-0.5 group-hover/ctr:left-0.5 group-hover/ctr:duration-100 dark:bg-dark dark:border-2 dark:border-yellow-200"></span>
+
+
+            <div
+                class="tip flex flex-col items-center gap-2 bg-white border border-solid border-yellow-800 rounded-lg p-4 dark:bg-dark dark:border-yellow-200 lg:flex-row lg:gap-4">
+
+                <div class="relative flex flex-col items-center gap-3 lg:flex-row">
+                    <svg class="inline w-10 h-10 shrink-0 lg:w-12 lg:h-12 fill-yellow-200 stroke-yellow-800 dark:fill-yellow-800 dark:stroke-yellow-200"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+                    </svg>
+
+
+                    <p class="text-justify [text-align-last:center] lg:[text-align-last:left] lg:text-lg">
+                        {!! $page->trans('featured-projects.notice') !!}
+                    </p>
+                </div>
+
+                <x-buttons.secondary text="{!! $page->trans('featured-projects.notice.button') !!}" class="shrink-0" href="#projetcs-archive"
+                    :reverseDirection="true">
+                    <svg class="w-5 h-5 fill-brown-500 group-hover:fill-yellow-50 group-hover:scale-125 group-hover:duration-300 dark:fill-yellow-200 dark:group-hover:fill-yellow-900"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+                    </svg>
+                </x-buttons.secondary>
+            </div>
         </div>
     </div>
 </div>
