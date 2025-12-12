135c530e625c1a337c9a90b356954fdd3f55b450
AmraniCh <elamrani.sv.laza@gmail.com>
2025-12-12 13:02:18 +0100

    style (vertical-link): add font weight changes on hover | update hero section link font weight

--- source/_components/vertical-link.blade.php
+++ source/_components/vertical-link.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ $link }}" target="blank"
-    class="link block text-brown-200 text-center cursor-pointer -rotate-90 tracking-widest-x2 uppercase hover:text-brown-600 hover:-rotate-45 hover:transition-all dark:text-brown-500 dark:hover:text-brown-100"
+    class="link block text-brown-200 text-center cursor-pointer -rotate-90 tracking-widest-x2 uppercase font-light hover:font-bold hover:text-brown-600 hover:-rotate-45 hover:transition-all dark:text-brown-500 dark:hover:text-brown-100"
     style="letter-spacing: 5px">
     {!! $text !!}
 </a>
