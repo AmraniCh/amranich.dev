9bac63ee293e6c05adcecea562162f794a08cd3b
AmraniCh <elamrani.sv.laza@gmail.com>
2026-08-29 19:56:11 +0100

    feat (projects-archive): add presentation links for FTPGuard and NeuralCodec-RPi

---
+++ source/_components/svg/presentation.blade.php
@@ -0,0 +1,5 @@
+<svg class="w-4 h-4 fill-yellow-800 hover:scale-110 hover:opacity-90 dark:fill-yellow-200"
+    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+    <path
+        d="M0 128C0 92.7 28.7 64 64 64H512c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zm176 96v72c0 13.3 10.7 24 24 24h176c13.3 0 24-10.7 24-24V224c0-13.3-10.7-24-24-24H200c-13.3 0-24 10.7-24 24z" />
+</svg>
